2d020279419ec9ca117b83602da1021b52d138a6
Nethsara <nethsarasiyum2@gmail.com>
2023-05-15 15:14:22 +0530

    file structure

---
+++ react/my-app/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RoundButton from "../../components/RoundButton/RoundButton";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <RoundButton name="hiiii" />
+    </div>
+  );
+};
+
+export default HomePage;
