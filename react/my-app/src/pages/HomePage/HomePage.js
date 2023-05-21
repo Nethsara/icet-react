2607a51f28f76fb7fbb5d22f844aac27c63f8a93
Nethsara <nethsarasiyum2@gmail.com>
2023-05-21 09:49:42 +0530

    useState

--- react/my-app/src/pages/HomePage/HomePage.js
+++ react/my-app/src/pages/HomePage/HomePage.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Button from "@mui/material/Button";
 
 export const HomePage = () => {
+  const [email, setEmail] = useState("");
+
   const clickFunction = () => {
     alert("I Clicked");
   };
@@ -15,6 +17,16 @@ export const HomePage = () => {
       </button>
       <button className="my-btn">My Button</button>
       <Button variant="contained">Hello World</Button>
+      <br />
+
+      <input
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
     </div>
   );
 };
