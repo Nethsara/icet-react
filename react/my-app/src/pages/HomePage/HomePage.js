4711e78a738893e81e64d0e291460e08e51108c4
Nethsara <nethsarasiyum2@gmail.com>
2023-05-21 11:33:58 +0530

    get method

--- react/my-app/src/pages/HomePage/HomePage.js
+++ react/my-app/src/pages/HomePage/HomePage.js
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Button from "@mui/material/Button";
 
 export const HomePage = () => {
-  const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [id, setID] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
 
   const clickFunction = () => {
     alert("I Clicked");
+  };
+
+  const saveData = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        body,
+        userId: id,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setBody("");
+        setTitle("");
+        setID("");
+      })
+      .catch((e) => console.error(e));
   };
 
   return (
@@ -20,13 +51,42 @@ export const HomePage = () => {
       <br />
 
       <input
-        type="email"
-        placeholder="Enter your email"
-        value={email}
+        type="text"
+        placeholder="Enter your title"
+        value={title}
         onChange={(e) => {
-          setEmail(e.target.value);
+          setTitle(e.target.value);
         }}
       />
+
+      <input
+        type="text"
+        placeholder="Enter your body"
+        value={body}
+        onChange={(e) => {
+          setBody(e.target.value);
+        }}
+      />
+
+      <input
+        type="number"
+        placeholder="Enter your id"
+        value={id}
+        onChange={(e) => {
+          setID(e.target.value);
+        }}
+      />
+
+      <button onClick={saveData}>Save</button>
+
+      {data.map((v) => {
+        return (
+          <div key={v.id} className="container">
+            <h1>{v.title}</h1>
+            <p>{v.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
